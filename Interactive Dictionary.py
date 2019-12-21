f14573337e80fd3fc6f18c1950fe6f70c751bae7
CharlesCromer <57553120+CharlesCromer@users.noreply.github.com>
2019-12-21 00:46:33 -0600

    Welcome, load json, translate()

--- Interactive Dictionary.py
+++ Interactive Dictionary.py
@@ -5,12 +5,22 @@
 # This is an interactive dictionary that will ask the user for a word to define
 # and print out the resulting definition(s)
 
-print('\n\n\n\n\n\n\n\nInteractive Dictionary\n')
-print('This program is an interactive dictionary that will allow you to')
-print('enter a word you wish to have defined. It will then produce the')
-print('definition(s).')
+import json
 
+# Welcome message
+def welcome():
+    print('\n\n\n\n\n\n\n\nInteractive Dictionary\n')
+    print('This program is an interactive dictionary that will allow you to')
+    print('enter a word you wish to have defined. It will then produce the')
+    print('definition(s).')
 
+def translate(word):
+    return data(word)
 
+welcome()
 
+#loads data.json file
+data = json.load(open('data.json'))
 
+word = input('Enter Word: ')
+print(translate(word))
