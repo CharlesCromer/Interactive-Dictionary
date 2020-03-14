0e1257f334ce314e76167680f7f9fb8bfef5187e
CharlesCromer <57553120+CharlesCromer@users.noreply.github.com>
2020-03-14 01:56:59 -0500

    Update Interactive Dictionary.py
    NOTE: Still need to debug if I hit enter or blanks for some of the inputs.
    
    1. Added validation for word entry to make sure it is alpha and not blank.
    
    2.  Added definitions on new line with def count.
    
    3. Changed some returns to prints

--- Interactive Dictionary.py
+++ Interactive Dictionary.py
@@ -1,20 +1,27 @@
 # Interactive Dictionary
 # Created by Charles Cromer
 # Created on 12/18/2019
-# Last Modified on 12/18/2019
+# Last Modified on 3/14/2020
 # This is an interactive dictionary that will ask the user for a word to define
 # and print out the resulting definition(s)
 
 import json
 from difflib import get_close_matches
 
-
 def main():
     welcome()
     runAgain = 'Y'  #Sets flag for "Run Again?" loop
     while runAgain.upper() == 'Y':  #Run Again loop
         word = input('Enter Word: ')
-        print(translate(word))
+
+        while not word.isalpha() or word == '':
+            word = input('Error, Please enter a word: ')
+
+        definition = (translate(word))
+        linenum = 1
+        for item in definition:
+            print(str(linenum) + ':', item)
+            linenum += 1
         runAgain = input('\nWould you like to define another word?  [Y/N]\n' )
     print('Thank you for using the interactive dictionary!')
 
@@ -32,16 +39,16 @@ def translate(word):
     if word in data:    #validates word is in .json file
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        similar = input("\nDid you mean [ %s ] instead? [Y/N]\n" % get_close_matches(word, data.keys())[0]).upper()
+        similar = input("\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word, data.keys())[0]).upper()
 
         while similar != 'Y' and similar != 'N':  # Validation Loop for y/n
-            similar = input("\nDid you mean [ %s ] instead? [Y/N]\n" % get_close_matches(word, data.keys())[0]).upper()
+            similar = input("\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word, data.keys())[0]).upper()
 
         if similar == 'Y':
             return data[get_close_matches(word, data.keys())[0]]
         else:
-            return "\nThe word doesn't exist. Please double check it.\n"
+            print("\nThe word doesn't exist. Please double check it.\n")
     else:
-        return "\nThe word doesn't exist. Please double check it.\n"
+        print("\nThe word doesn't exist. Please double check it.\n")
 
 main()
