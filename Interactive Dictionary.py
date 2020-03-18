481214b593671aa27ccdcbb829099444400586a9
CharlesCromer <57553120+CharlesCromer@users.noreply.github.com>
2020-03-18 16:23:27 -0500

    starting to add mysql functionality

--- Interactive Dictionary.py
+++ Interactive Dictionary.py
@@ -9,13 +9,20 @@ import json
 import mysql.connector
 from difflib import get_close_matches
 
+
 def dBase():
-    con - mysql.connector.connect(
-        user = "ardit700_student",
-        password = 'ardit00_student',
-        host = '100.167.148.122',
-        database = 'ardit700_pm1database'
-    )
+    con = mysql.connector.connect(
+        user="ardit700_student",
+        password="ardit700_student",
+        host="108.167.140.122",
+        database="ardit700_pm1database")
+
+    cursor = con.cursor()
+
+    query = cursor.execute("SELECT * FROM Dictionary")
+    results = cursor.fetchall()
+    print(results)
+
 
 def main():
     welcome()
@@ -44,13 +51,13 @@ def welcome():
 def translate(word):
     data = json.load(open("data.json"))  # Loads dictionary into 'data'
     if word.lower() in data:  # validates word is in .json file
-        definition = data[word]
+        definition = data[word.lower()]
     elif word.title() in data:  # Checks for proper nouns
         definition = data[word.title()]
     elif word.upper() in data:  # Checks for Acronyms
         definition = data[word.upper()]
-    elif len(get_close_matches(word.lower(), data.keys())) > 0\
-            or len(get_close_matches(word.title(), data.keys())) > 0\
+    elif len(get_close_matches(word.lower(), data.keys())) > 0 \
+            or len(get_close_matches(word.title(), data.keys())) > 0 \
             or len(get_close_matches(word.upper(), data.keys())) > 0:
         definition = similarWord(data, word)
     else:
@@ -65,6 +72,8 @@ If will ask the user if the word that matches the pattern and case the closest i
 If not it will find the closest matching word that doesn't match case if there is one.
 Example: If you enter Pariss, it will first see if you meant Paris, if not it will see if you meant piss.
 '''
+
+
 def similarWord(data, word):
     if word.islower():
         similar = input("\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word, data.keys())[0]).upper()
@@ -80,7 +89,8 @@ def similarWord(data, word):
                     "\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[0]).upper()
                 while similar != 'Y' and similar != 'N':  # Validation Loop for y/n
                     similar = input(
-                        "\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[0]).upper()
+                        "\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[
+                            0]).upper()
                 if similar == 'Y':
                     definition = data[get_close_matches(word.title(), data.keys())[0]]
                 else:
@@ -88,10 +98,12 @@ def similarWord(data, word):
         else:
             definition = "\nThe word doesn't exist. Please double check it.\n"
     elif word.istitle():
-        similar = input("\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[0]).upper()
+        similar = input(
+            "\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[0]).upper()
 
         while similar != 'Y' and similar != 'N':  # Validation Loop for y/n
-            similar = input("\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[0]).upper()
+            similar = input(
+                "\nDid you mean [ %s ] instead? [Y/N]" % get_close_matches(word.title(), data.keys())[0]).upper()
 
         if similar == 'Y':
             definition = data[get_close_matches(word.title(), data.keys())[0]]
